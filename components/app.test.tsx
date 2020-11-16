c14b5a2ac0da44cc86c657e6af9410a5a245e182
Murad <murad.guliyev@pashabank.az>
2020-11-16 10:59:55 +0400

    Fixed test environment

--- components/app.test.tsx
+++ components/app.test.tsx
@@ -3,10 +3,30 @@ import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 
+import { IEmployee } from "../helpers/models";
 import App from "./app";
 
+const mockEmployees: IEmployee[] = [
+    {
+        id: "1",
+        name: "Name One",
+        surname: "Surname One",
+        dateOfBirth: "01-01-1991",
+        position: "Position One",
+        phoneNumber: "+994511111",
+    },
+    {
+        id: "2",
+        name: "Name Two",
+        surname: "Surname Two",
+        dateOfBirth: "02-02-1992",
+        position: "Position Two",
+        phoneNumber: "+994511112",
+    },
+];
+
 test("search works right", () => {
-    render(<App />);
+    render(<App employeesList={mockEmployees} />);
     const searchInput = screen.getByRole("textbox");
     expect(screen.getByText(/surname one/i)).toBeInTheDocument();
     expect(screen.getByText(/surname two/i)).toBeInTheDocument();
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    setupFilesAfterEnv: ["./jest.setup.js"],
     testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
     transform: {
         "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
